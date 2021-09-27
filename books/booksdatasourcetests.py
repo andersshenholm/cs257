8698484a2384f04599390662bd18b91a6158dcce
hempel-costello.simon20@libertycommon.org <hempel-costello.simon20@libertycommon.org>
2021-09-26 21:38:06 -0500

    First Unit Test Implementation

---
+++ books/booksdatasourcetests.py
@@ -0,0 +1,98 @@
+'''
+   booksdatasourcetest.py
+   Jeff Ondich, 24 September 2021
+'''
+
+import booksdatasource
+import unittest
+
+class BooksDataSourceTester(unittest.TestCase):
+    def setUp(self):
+        self.data_source = booksdatasource.BooksDataSource('books1.csv')
+
+    def tearDown(self):
+        pass
+
+    def test_unique_author_len(self):
+        authors = self.data_source.authors('Pratchett')
+        self.assertTrue(len(authors) == 1)
+
+    def test_unique_author_name(self):
+        authors = self.data_source.authors('Pratchett')
+        self.assertTrue(authors[0] == booksdatasource.Author('Pratchett', 'Terry'))
+
+    def test_unique_title_single_author_len(self):
+        books = self.data_source.books('Blackout')
+        self.assertTrue(len(books)== 1)
+
+    def test_unique_title_single_author_name(self):
+        books = self.data_source.books('Blackout')
+        author_name = 'Connie Willis'
+        author = booksdatasource.Author('Pratchett', 'Terry')
+        author_list = [author]
+        self.assertTrue(books[0] == booksdatasource.Book('Blackout',2010,author_list))
+
+    def test_unique_title_multiple_author_len(self):
+        books = self.data_source.books('Good Omens')
+        self.assertTrue(len(books)==1)
+
+    def test_unique_title_multiple_author_names(self):
+        books = self.data_source.books('Good Omens')
+        author_neil = booksdatasource.Author('Gaiman', 'Neil')
+        author_pratchett = booksdatasource.Author('Pratchett','Terry')
+        author_list = [author_neil, author_pratchett]
+        self.assertTrue(books[0] == booksdatasource.Book('Good Omens', 1990, author_list))
+
+    def test_null_title_entry(self):
+        self.assertRaises(ValueError, self.data_source.books, '')
+
+    def test_null_author_entry(self):
+        self.assertRaises(ValueError, self.data_source.authors,'')
+    def test_null_date_entry(self):
+        self.assertRaises(ValueError, self.data_source.books_between_years,'')
+    def test_non_unique_title_len(self):
+        books = self.data_source.books('street')
+        self.assertTrue(len(books)==2)
+
+    def test_non_unique_title_names_alphabetical(self):
+        books = self.data_source.books('street', 'title')
+        author_lewis = booksdatasource.Author('Lewis', 'Sinclair')
+        author_baldwin = booksdatasource.Author('Baldwin','James')
+        author_list_main_street = [author_lewis]
+        author_list_beale = [author_baldwin]
+        self.assertTrue((books[0]  == booksdatasource.Book('If Beale Street Could Talk', 1974, author_list_beale)) and (books[1] == booksdatasource.Book('Main Street', 1920, author_list_main_street)))
+
+    def test_non_unique_title_names_chronological(self):
+        books = self.data_source.books('street', 'year')
+        author_lewis = booksdatasource.Author('Lewis', 'Sinclair')
+        author_baldwin = booksdatasource.Author('Baldwin','James')
+        author_list_main_street = [author_lewis]
+        author_list_beale = [author_baldwin]
+        self.assertTrue((books[1]  == booksdatasource.Book('If Beale Street Could Talk', 1974, author_list_beale)) and (books[0] == booksdatasource.Book('Main Street', 1920, author_list_main_street)))
+
+    def test_non_unique_author_len(self):
+        authors = self.data_source.authors('Brontë')
+        self.assertTrue(len(authors) == 3)
+
+    def test_non_unique_author_name(self):
+        authors = self.data_source.authors('Pratchett')
+        self.assertTrue(authors[0] == booksdatasource.Author('Brontë', 'Ann')and authors[1] == booksdatasource.Author('Brontë', 'Charlotte') and authors[2] ==booksdatasource.Author('Brontë', 'Emily'))
+    def test_date_return_unique_len(self):
+        books = self.data_source.books_between_years(1848, 1849)
+        self.assertTrue(len(books) == 0)
+    def test_date_return_unique_name(self):
+        books = self.data_source.books_between_years(1848, 1849)
+        author = booksdatasource.Author('Bronte', 'Ann')
+        author_list = [author]
+        self.assertTrue(books[0] == booksdatasource.Book('The Tenant of Wildfell Hall',1848,author_list))
+    def test_date_return_unique_name(self):
+        books = self.data_source.books_between_years(2020,2020)
+        author_schwab = booksdatasource.Author('Schwab', 'V.E.')
+        author_list_schwab = [author_schwab]
+        author_orenstein = booksdatasource.Author('Orenstein', 'Peggy')
+        author_list_orenstein = [author_orenstein]
+        self.assertTrue(books[0] == booksdatasource.Book('Boys and Sex',2020,author_list_orenstein) and books[1] ==booksdatasource.Book('The Invisible Life of Addie LaRue',2020,author_list_schwab))
+
+if __name__ == '__main__':
+    unittest.main()
+
