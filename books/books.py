c86656bb8a2fd2bb1e23a7aba1688e2a5367a4e6
Anders Shenholm <andersshenholm@Anderss-MacBook-Air.local>
2021-10-01 01:41:59 -0500

    beginings of cli with sys.argv

--- books/books.py
+++ books/books.py
@@ -1,24 +1,43 @@
 import booksdatasource
 #from what I can tell this is the best option for processing cli arguments
-import argparser
+import sys
 
-parser = argparse.ArgumentParser(description='Process booksdatasource requests')
-parser.add_argument('authorsearch', 'as', help='searches by author')
-parser.add_argument('titlesearch', 'ts', help='searches by title')
-parser.add_argument('datesearch', 'ds', help='searches by date')
-parser.add_argument('-t', '--sorttitle', help='sorts books by title')
-parser.add_argument('-d', '--sortdate', help='sorts books by date')
+def parse_args():
+    args = sys.argv
+    
+    if args exactly 2 in length and one arg asks for help, 
+        print_usage()
+        return
+    
 
-args = parser.parse_args()
+    
+    authorsearch, titlesearch, datesearch = None, None, None
 
-def as(search_string):
-    searched_authors[] = booksdatasource.authors 
-    return_string = ""
-    for each_author in authorlist:
-        if each_author in booksdatasource.authors(search_string)
-        return_string += each_author
-        #here we need to get books written by some author. Parse from csv doc or is this somehow attached to authors in booksdatasource?
+    commandcount = 0
+    if 'titlesearch' in args:
+            commandcount+=1
+    if 'ts' in args:
+            commandcount+=1
+    if 'authorsearch' in args:
+            commandcount+=1
+    if 'as' in args:
+            commandcount+=1
+    if 'datesearch' in args:
+            commandcount+=1
+    if 'ds' in args:
+            commandcount+=1
+    print(commandcount)
+    if commandcount != 1:
+        print("The books program accepts exactly one command argument. Access the usage document with -h, --help")
+       
+                  
+parse_args()
 
-authorsearch = as
+def print_usage():
+     with open("usage.txt") as f:
+        print (f.read())
 
-def
+
+
+#print(args)
+#print("Argument List:", str(sys.argv))
