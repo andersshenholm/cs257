bdf026e49ed29935164879de9e5a5448fb243042
Anders Shenholm <andersshenholm@Anderss-MacBook-Air.local>
2021-10-22 13:02:45 -0500

    added last query

--- olympics/queries.sql
+++ olympics/queries.sql
@@ -30,23 +30,16 @@ ORDER BY results.games;
 /*
 List all the NOCs and the number of gold medals they have won, in decreasing order of the number of gold medals.
 
-
-------WORK IN PROGRESS----------
-
-SELECT teams.noc, 
-FROM teams, results, athletes
-ORDER BY teams.noc;
-
-WHERE results.medal = 'Gold'
-AND results.name
-search results
-IF medal = 'Gold' 
-    important name = name
-    important games = games
-    
-search athletes:
-if important name = name
-   and important games = games
-
-
+2 things:
+1) This query lists total medals awarded to athletes of an noc (ex. If a team of 4 wins a relay race, their noc's gold medal count increases by 4)
+2) This query assumes that every noc has either won a gold medal (WHERE results.medal = 'Gold') or has for at least one event not won a medal ("OR results.medal IS NULL"). It works properly for this dataset. However, if a new noc comes on the scene and only wins silver and bronze, they won't be listed with other 0-time gold medal winners.
 */
+
+
+SELECT COUNT (results.medal), results.noc 
+FROM results
+WHERE results.medal = 'Gold'
+OR results.medal IS NULL
+GROUP BY results.noc
+ORDER BY COUNT(results.medal) DESC;
+
